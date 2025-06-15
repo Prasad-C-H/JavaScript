d10f5acf15e9c72f96f0cc5aec367c201b917e1e
Prasad CH <116002750+Prasad-C-H@users.noreply.github.com>
2025-06-15 12:02:48 +0000

    var and data types

---
+++ 01basics/01_variables.js
@@ -0,0 +1,3 @@
+console.log("Prasad")
+
+console.log(["Prasad"])
---
+++ 01basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // we are using node.js not browser
+
+console.log(3 + 3); 
+
+console.log("Prasad");
+
+/*
+Primitive Data Types
+number = 2 to pow 53
+bigint
+string = ""
+boolean = t/f
+null = standalone value
+undefined = no value assigned
+symbol
+
+
+Object
+
+
+
+*/
